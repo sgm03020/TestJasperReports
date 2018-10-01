b538e466aa184a203e90c61f015272d4eb2c2b02
tsuyoshi <airtk2000@gmail.com>
2018-10-01 11:06:22 +0900

    first commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+JasperReportのjava出力サンプル
+
+
+参考にした情報
+JasperReportsで帳票出力してみた
+https://dev.classmethod.jp/server-side/java/jasperreports-tutorial/
+
+
---
+++ src/main/java/examples/JasperMain.java
@@ -0,0 +1,28 @@
+package examples;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRCsvDataSource;
+
+public class JasperMain {
+	  public static void main(String[] args) throws Exception {
+		    Map<String, Object> parameters = new HashMap<>();
+		    JRCsvDataSource dataSource = new JRCsvDataSource(JasperMain.class.getResourceAsStream("data.csv"));
+		    dataSource.setUseFirstRowAsHeader(true);
+		    try (InputStream template = JasperMain.class.getResourceAsStream("sample.jrxml")) {
+		      JasperReport report = JasperCompileManager.compileReport(template);
+		      JasperPrint jasperPrint = JasperFillManager.fillReport(report, parameters, dataSource);
+		      JasperExportManager.exportReportToPdfFile(jasperPrint, "pdf/sample.pdf");
+		    }
+		    finally {
+				System.out.println("終了しました");
+			}
+		  }
+}
